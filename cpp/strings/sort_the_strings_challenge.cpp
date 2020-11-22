7bd9d20cfb5ee90ce992ef14c125a2a1dd51a49f
Rashik Ansar <sk.ansar46@gmail.com>
2020-11-22 17:51:11 +0530

    [cpp/strings] sort the string challenge

---
+++ cpp/strings/sort_the_strings_challenge.cpp
@@ -0,0 +1,100 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+/*
+SAMPLE INPUT:00
+3
+92 022
+82 12
+77 13
+2 false numeric
+
+SAMPLE OUTPUT
+82 12
+77 13
+92 022
+*/
+
+// extract required token
+string extractStringAtKey(string str, int key) {
+  char *s = strtok((char *)str.c_str(), " ");
+
+  while (key > 1) {
+    s = strtok(NULL, " ");
+    key--;
+  }
+  return (string)s;
+}
+
+int convertToInt(string s) {
+  int ans = 0;
+  int p = 1;
+  for (int i = s.length() - 1; i >= 0; i--) {
+    ans += ((s[i] - '0') * p);
+    p = p * 10;
+  }
+  return ans;
+}
+
+// comparaters
+bool numericCompare(pair<string, string> s1, pair<string, string> s2) {
+  string key1, key2;
+  key1 = s1.second;
+  key2 = s2.second;
+
+  return convertToInt(key1) < convertToInt(key2);
+}
+
+bool lexicoCompare(pair<string, string> s1, pair<string, string> s2) {
+  string key1, key2;
+  key1 = s1.second;
+  key2 = s2.second;
+
+  return key1 < key2;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  cin.get();
+
+  string a[n];
+  for (int i = 0; i < n; i++) {
+    getline(cin, a[i]);
+  }
+
+  int key;
+  string reversal, ordering;
+  cin >> key >> reversal >> ordering;
+
+  pair<string, string> strPair[100];
+
+  for (int i = 0; i < n; i++) {
+    strPair[i].first = a[i];
+    strPair[i].second = extractStringAtKey(a[i], key);
+  }
+
+  // sort
+  if (ordering == "numeric") {
+    sort(strPair, strPair + n, numericCompare);
+  } else {
+    sort(strPair, strPair + n, lexicoCompare);
+  }
+
+  // Reversal
+  if (reversal == "true") {
+    for (int i = 0; i < n / 2; i++) {
+      swap(strPair[i], strPair[n - i - 1]);
+    }
+  }
+
+  // Print the output
+  for (int i = 0; i < n; i++) {
+    cout << strPair[i].first << endl;
+  }
+
+  return 0;
+}

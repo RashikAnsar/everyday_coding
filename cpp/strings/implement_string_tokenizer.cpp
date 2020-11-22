d6da233eecb83d96318ac9516441b32f841db85f
Rashik Ansar <sk.ansar46@gmail.com>
2020-11-22 12:43:32 +0530

    [cpp/strings] string tokenization

---
+++ cpp/strings/implement_string_tokenizer.cpp
@@ -0,0 +1,46 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+char *mystrtok(char *s, char delim) {
+  static char *input = NULL;
+  if (s != NULL) {
+    input = s;
+  }
+
+  // base case: After the final token has retured
+  if (input == NULL) {
+    return NULL;
+  }
+
+  // extraction tokens and storing them in an array
+  char *output = new char[strlen(input) + 1];
+  int i = 0;
+  for (; input[i] != '\0'; i++) {
+    if (input[i] != delim) {
+      output[i] = input[i];
+    } else {
+      output[i] = '\0';
+      input = input + i + 1;
+      return output;
+    }
+  }
+  // corner case
+  output[i] = '\0';
+  input = NULL;
+  return output;
+}
+
+int main() {
+  char s[100] = "Today is a rainy day";
+  char *ptr = mystrtok(s, ' ');
+  cout << ptr << endl;
+
+  while (ptr != NULL) {
+    ptr = mystrtok(NULL, ' ');
+    cout << ptr << endl;
+  }
+
+  return 0;
+}
---
+++ cpp/strings/string_tokenization.cpp
@@ -0,0 +1,18 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char s[100] = "Today is a rainy day";
+
+  char *ptr = strtok(s, " ");
+  cout << ptr << endl;
+
+  while (ptr != NULL) {
+    ptr = strtok(NULL, " ");
+    cout << ptr << endl;
+  }
+
+  return 0;
+}

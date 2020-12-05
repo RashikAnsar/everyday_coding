099a251bddb5ff3ae00cce9a4ba4dec0e56b105d
Rashik Ansar <sk.ansar46@gmail.com>
2020-12-05 23:00:21 +0530

    [cpp/STL] lower boud

---
+++ cpp/STL/lower_bound.cpp
@@ -0,0 +1,23 @@
+// QUESTION: https://www.interviewbit.com/problems/upper-lower-bound/
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+  }
+  int q;
+  cin >> q;
+  for (int i = 0; i < q; i++) {
+    int x;
+    cin >> x;
+    int idx = lower_bound(v.begin(), v.end(), x) - v.begin();
+    cout << idx << endl;
+  }
+}

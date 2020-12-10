eea72044a05e45e8ed5ab0a4e5f6c3d45b2f3cf8
Rashik Ansar <sk.ansar46@gmail.com>
2020-12-10 23:08:30 +0530

    [cpp/bit_manipulation] clear range of bits

---
+++ cpp/bit_manipulation/clear_range_of_bits.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// clears last i bits of number n
+int clearLastIBits(int n, int i) {
+  int mask = (-1 << i);
+  return n & mask;
+}
+
+// clear range of bits between i and j
+int clearRangeItoJ(int n, int i, int j) {
+  int ones = (~0);
+  int a = ones << (j + 1);
+  int b = (1 << j) - 1;
+  int mask = a | b;
+  return n & mask;
+}
+int main() {
+  int n = 31;
+  cout << clearLastIBits(n, 3) << endl;
+  cout << clearRangeItoJ(n, 1, 3) << endl;
+}
